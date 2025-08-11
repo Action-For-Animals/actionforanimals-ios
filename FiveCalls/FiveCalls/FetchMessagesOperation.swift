5963350d29c204c9992411fbd2012e8e4d52992d
riya-235 <riyarpatel08@gmail.com>
2025-08-11 16:47:56 -0700

    removing fetch messages operation

--- FiveCalls/FiveCalls/FetchMessagesOperation.swift
+++ FiveCalls/FiveCalls/FetchMessagesOperation.swift
@@ -25,52 +25,13 @@ class FetchMessagesOperation: BaseOperation, @unchecked Sendable {
     }
     
     var url: URL {
-        var components = URLComponents(string: "https://api.5calls.org/v1/users/inbox")
-        let districtQueryParam = URLQueryItem(name: "district", value: district)
-        components?.queryItems = [districtQueryParam]
-        return components!.url!
+        // Not used anymore
+        return URL(string: "temp")!
     }
     
     override func execute() {
-        let request = buildRequest(forURL: url)
-        
-        let task = session.dataTask(with: request) { data, response, error in
-            if let e = error {
-               self.error = e
-            } else {
-                self.handleResponse(data: data, response: response)
-            }
-            self.finish()
-        }
-        task.resume()
-    }
-    
-    private func handleResponse(data: Data?, response: URLResponse?) {
-        guard let data = data else { return }
-        guard let http = response as? HTTPURLResponse else { return }
-        
-        httpResponse = http
-        
-        if http.statusCode == 200 {
-            do {
-                self.messages = try parseMessages(data: data)
-            } catch let e {
-                print("Error parsing messages: \(e.localizedDescription)")
-            }
-        } else {
-            print("Received HTTP \(http.statusCode)")
-        }
-    }
-    
-    private func parseMessages(data: Data) throws -> [InboxMessage] {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
-        let messagesResponse = try decoder.decode(MessagesRespsonse.self, from: data)
-
-        return messagesResponse.messages
-    }
-    
-    struct MessagesRespsonse: Decodable {
-        var messages: [InboxMessage]
+        // Skip network call, return empty messages immediately
+        self.messages = []
+        self.finish()
     }
 }
